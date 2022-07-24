abca01ab407f142387cddcb15fd43b51bdaf4ad4
Wilson Tian <wilson.tian@perkinelmer.com>
2022-07-24 11:07:37 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:jest-formatting/recommended'],
+  env: {
+    jest: true,
+  },
+  globals: {
+    // detox
+    detox: 'readonly',
+    device: 'readonly',
+    expect: 'readonly',
+    waitFor: 'readonly',
+    element: 'readonly',
+    by: 'readonly',
+  },
 };
