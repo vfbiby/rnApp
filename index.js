2d2548450dae79e17739c4c0335a9626af22d480
James Zhang <liulfbaby@gmail.com>
2022-07-24 10:08:05 +0800

    Use SafeAreaView.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import App from "./App";
-import Example from "./src/Example";
+import { UserSignupPage } from "./src/UserSignupPage";
 
 // AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => Example);
+AppRegistry.registerComponent(appName, () => UserSignupPage);
--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -1,15 +1,15 @@
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, SafeAreaView, Text, TextInput, View } from "react-native";
 import React from "react";
 
 export function UserSignupPage() {
   return (
-    <View>
+    <SafeAreaView>
       <Text className={"text-2xl"}>Sign Up</Text>
       <TextInput placeholder={"Your display name"} />
       <TextInput placeholder={"Your username"} />
       <TextInput secureTextEntry={true} placeholder={"Your password"} />
       <TextInput secureTextEntry={true} placeholder={"Repeat your password"} />
       <Button title={"Button"} />
-    </View>
+    </SafeAreaView>
   );
 }
