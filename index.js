1614399d2393fba4a0d00bbaeeca1f3cd39aa65b
James Zhang <liulfbaby@gmail.com>
2022-07-24 19:48:09 +0800

    Import UserSignup component with a wrapper

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
  * @format
  */
 
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import App from './App';
 import { UserSignupPage } from './src/UserSignupPage';
 import Example from './src/Example';
 
+const Wrapper = () => {
+  return (
+    <UserSignupPage
+      actions={{
+        postSignup: () => {},
+      }}
+    />
+  );
+};
+
 // AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => UserSignupPage);
+AppRegistry.registerComponent(appName, () => Wrapper);
