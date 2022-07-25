9e32b587431fec19c2419448c91f6d328887c7c3
James Zhang <liulfbaby@gmail.com>
2022-07-25 19:45:16 +0800

    TDD Input Custom Component.

---
+++ src/Components/Input.js
@@ -0,0 +1,28 @@
+import { Text, TextInput, View } from 'react-native';
+import React from 'react';
+
+export function Input(props) {
+  let className: string = '';
+  if (props.hasError !== undefined) {
+    className = props.hasError ? 'is-invalid' : 'is-valid';
+  }
+  if (props.className) {
+    className += className === '' ? props.className : ' ' + props.className;
+  }
+  return (
+    <View>
+      {props.label && <Text>{props.label}</Text>}
+      <TextInput
+        className={className}
+        class={className}
+        onChangeText={props.onChange}
+        placeholder={props.placeholder}
+        value={props.value}
+        type={props.type || 'text'}
+        secureTextEntry={props.type}
+        testID={'input'}
+      />
+      {props.hasError && <Text>{props.error}</Text>}
+    </View>
+  );
+}
--- src/Components/Input.spec.js
+++ src/Components/Input.spec.js
@@ -1,19 +1,92 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react-native';
-import { TextInput, View } from 'react-native';
-
-function Input() {
-  return (
-    <View>
-      <TextInput />
-    </View>
-  );
-}
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import { Input } from './Input';
 
 describe('Layout', function () {
   it('should has input item', function () {
     render(<Input />);
-    const input = screen.queryByRole('input');
+    const input = screen.queryByTestId('input');
     expect(input).toBeTruthy();
   });
+
+  it('should display the label provided in props', function () {
+    render(<Input label={'Test label'} />);
+    const label = screen.queryByText('Test label');
+    expect(label).toBeTruthy();
+  });
+
+  it('should not display the label when no label provided in props', function () {
+    render(<Input />);
+    const label = screen.queryByText('Test label');
+    expect(label).toBeNull();
+  });
+
+  it('should has text type for input when type is not provided as prop', function () {
+    render(<Input />);
+    const input = screen.queryByTestId('input');
+    expect(input.props.type).toBe('text');
+  });
+
+  it('should has password type for input when password type is provided as prop', function () {
+    render(<Input type={'password'} />);
+    const input = screen.queryByTestId('input');
+    expect(input.props.secureTextEntry).toBeTruthy();
+    expect(input.props.type).toBe('password');
+  });
+
+  it('should display placeholder when it is provided as prop', function () {
+    render(<Input placeholder={'Test placeholder'} />);
+    const input = screen.queryByTestId('input');
+    expect(input.props.placeholder).toBe('Test placeholder');
+  });
+
+  it('should has value for input when it is provided as prop', function () {
+    render(<Input value={'Test value'} />);
+    const input = screen.queryByTestId('input');
+    expect(input.props.value).toBe('Test value');
+  });
+
+  it('should has onChange callback when it is provided as prop', function () {
+    const onChange = jest.fn();
+    render(<Input onChange={onChange} />);
+    const input = screen.queryByTestId('input');
+    fireEvent.changeText(input, 'Changed value');
+    expect(onChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('should has not default style when there is no validation error or success', function () {
+    render(<Input />);
+    const input = screen.queryByTestId('input');
+    expect(input.props.class).toBe('');
+  });
+
+  it('should has success style hasError properties false', function () {
+    render(<Input hasError={false} />);
+    const input = screen.queryByTestId('input');
+    expect(input.props.class).toBe('is-valid');
+  });
+
+  it('should has style for error when there is error', function () {
+    render(<Input hasError={true} />);
+    const input = screen.queryByTestId('input');
+    expect(input.props.class).toBe('is-invalid');
+  });
+
+  it('should displays the error text when it is provided', function () {
+    render(<Input hasError={true} error={'Cannot be null'} />);
+    expect(screen.queryByText('Cannot be null')).toBeTruthy();
+  });
+
+  it('should not displays the error text when hasError not provided', function () {
+    render(<Input error={'Cannot be null'} />);
+    expect(screen.queryByText('Cannot be null')).toBeNull();
+  });
+
+  it.skip('should has class style when it is provided', function () {
+    render(<Input className={'rounded'} />);
+    const input = screen.queryByTestId('input');
+    expect(input.props.class).toBe('rounded');
+  });
+
+  it('should ', function () {});
 });
